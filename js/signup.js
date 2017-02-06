1ce1e509cd99c76a199afb660343dd8f072ff725
Sonia Wu <swu52@hawk.iit.edu>
2017-02-06 17:07:42 -0600

    add success message at form submit

--- js/signup.js
+++ js/signup.js
@@ -1,20 +1,14 @@
-// Tell jQuery to give up the dollar sign
 $.noConflict();
 
 // jQuery 3.x-style ready event and locally scoped $
 jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
-
-  $('#email').on('keyup focus blur', function() {
-    var currentValue = $(this).val();
-    var validPattern = $(this).attr('pattern');
-    console.log(currentValue,validPattern);
-    if(currentValue.match(validPattern)) {
-      $('#submit').addClass('active');
-    } else {
-      $('#submit').removeClass('active');
-    }
-  });
+  
+  $('#signup-form').on('submit',function() {
+    var successMsg = '<p id="success-msg">You have successfuly signed up for the running event.</p>';
+    $('#signup-form').append(successMsg);
+    //console.log($('#name').val(),"  <end here>");
+  })
 
 });
