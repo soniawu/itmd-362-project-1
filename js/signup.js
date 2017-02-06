7f04a9c89ea349d6dbca9533ce7551adca93f15b
Sonia Wu <swu52@hawk.iit.edu>
2017-02-06 17:24:02 -0600

    Add field validation on blank fields.

--- js/signup.js
+++ js/signup.js
@@ -7,6 +7,22 @@ jQuery(function($) {
   
   $('#signup-form').on('submit',function() {
     var successMsg = '<p id="success-msg">You have successfuly signed up for the running event.</p>';
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var phone = $('#phone').val();
+   
+    if (name === '') {
+      alert('Name can not be blank.');
+      return false;
+    }
+    if (email === '') {
+      alert('Email can not be blank.');
+      return false;
+    }
+    if (phone === '') {
+      alert('Phone can not be blank.');
+      return false;
+    }
     $('#signup-form').append(successMsg);
     //console.log($('#name').val(),"  <end here>");
   })
