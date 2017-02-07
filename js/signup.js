1581be84137b79206613907af57c3182c5a015e7
Sonia Wu <swu52@hawk.iit.edu>
2017-02-07 17:51:07 -0600

    Add respond message after submit

--- js/signup.js
+++ js/signup.js
@@ -1,30 +1,36 @@
 $.noConflict();
 
 // jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
-  
-  $('#signup-form').on('submit',function() {
-    var successMsg = '<p id="success-msg">You have successfuly signed up for the running event.</p>';
-    var name = $('#name').val();
-    var email = $('#email').val();
-    var phone = $('#phone').val();
-   
-    if (name === '') {
-      alert('Name can not be blank.');
-      return false;
-    }
-    if (email === '') {
-      alert('Email can not be blank.');
-      return false;
-    }
-    if (phone === '') {
-      alert('Phone can not be blank.');
-      return false;
-    }
-    $('#signup-form').append(successMsg);
-    //console.log($('#name').val(),"  <end here>");
-  })
-
-});
+(function($) {
+  $(document).ready(
+    function() {
+      $('html').removeClass('nojs');
+      $('html').addClass('hasjs');
+      
+      $('#signup-form').on('submit', function(e) {
+        var successMsg = '<p id="success-msg">You have successfuly signed up for the running event.</p>'; 
+        var name = $('#name').val();
+        var email = $('#email').val();
+        var phone = $('#phone').val();
+       /* var currentValue = $('#email').val();
+        var validPattern = $('#email').attr('pattern');
+        console.log(currentValue,validPattern); */
+       
+        if (name === '') {
+          alert('Name can not be blank.');
+          return false;
+        }
+        if (email === '') {
+          alert('Email can not be blank.');
+          return false;
+        }
+        if (phone === '') {
+          alert('Phone can not be blank.');
+          return false;
+        }
+        $('#signup-form').append(successMsg);
+        console.log($('#name').val(), "  <end here>");
+        e.preventDefault();
+      });
+    })
+})(jQuery);
