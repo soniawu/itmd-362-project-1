90bc63b06bc4dc0881fa49b69cbb739c1bad7ba2
Sonia Wu <swu52@hawk.iit.edu>
2017-02-20 22:00:08 -0600

    reposition header text, misc color changes

--- js/signup.js
+++ js/signup.js
@@ -8,12 +8,13 @@ $.noConflict();
       $('html').addClass('hasjs');
       
       $('#signup-form').on('submit', function(e) {
-        var successMsg = '<p id="success-msg">You have successfuly signed up for the running event.</p>'; 
+         
         var name = $('#name').val();
         var email = $('#email').val();
         var phone = $('#phone').val();
         var emailCurrentValue = $('#email').val();
         var emailValidPattern = $('#email').attr('pattern');
+        var successMsg = '<p id="rspn-msg">'+ name + ', thank you for signup the running event. Email will be sent to you for the login process</p>';
          
         if (name === '') {
           alert('Name can not be blank.');
